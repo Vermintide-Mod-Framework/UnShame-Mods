cf14a6b6cff21ef46369d6a580d4ede7a73a6d1b
UnShame <mrunshame@gmail.com>
2018-06-19 04:13:15 +0300

    Huntsman_fix new code

--- huntsman_fix/scripts/mods/huntsman_fix/huntsman_fix.lua
+++ huntsman_fix/scripts/mods/huntsman_fix/huntsman_fix.lua
@@ -1,5 +1,18 @@
 local mod = get_mod("huntsman_fix")
 
+-- Removes empty (false) buff entries from the ends of event buff tables
+local function cleanup_event_buffs(self)
+	for _, event_buffs in pairs(self._event_buffs) do
+		while true do
+			if event_buffs[#event_buffs] == false then
+				table.remove(event_buffs, #event_buffs)
+			else
+				break
+			end
+		end
+	end
+end
+
 mod:hook_origin(BuffExtension, "remove_buff", function (self, id, handled_in_buff_update_function)
 	local buffs = self._buffs
 	local num_buffs = #buffs
@@ -25,6 +38,9 @@ mod:hook_origin(BuffExtension, "remove_buff", function (self, id, handled_in_buf
 			i = i + 1
 		end
 	end
+
+	-- Try removing empty buff entries from the ends of buff tables
+	cleanup_event_buffs(self)
 
 	return num_buffs_removed
 end)
@@ -58,7 +74,9 @@ mod:hook_origin(BuffExtension, "_remove_sub_buff", function (self, buff, index, 
 		local event = template.event
 		local event_buff_index = buff.event_buff_index
 
-		table.remove(self._event_buffs[event], event_buff_index)
+		-- Instead of using table.remove, set the buff entry to false
+		-- This makes sure that other event_buff_indexs point to the right buffs
+		self._event_buffs[event][event_buff_index] = false
 	end
 
 	table.remove(self._buffs, index)
@@ -103,14 +121,18 @@ mod:hook_origin(BuffExtension, "trigger_procs", function (self, event, ...)
 
 	for i = 1, num_event_buffs, 1 do
 		local buff = event_buffs[i]
-		local proc_chance = buff.proc_chance or 1
+		
+		-- Check if the buff entry isn't empty
+		if buff then
+			local proc_chance = buff.proc_chance or 1
 
-		if math.random() <= proc_chance then
-			local buff_func = buff.buff_func
-			local success = buff_func(player, buff, params)
+			if math.random() <= proc_chance then
+				local buff_func = buff.buff_func
+				local success = buff_func(player, buff, params)
 
-			if success and buff.template.remove_on_proc then
-				event_buffs_to_remove[#event_buffs_to_remove + 1] = buff
+				if success and buff.template.remove_on_proc then
+					event_buffs_to_remove[#event_buffs_to_remove + 1] = buff
+				end
 			end
 		end
 	end
